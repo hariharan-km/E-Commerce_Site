9a970d3a8e67e3465834adb1bbcfa1a49c2bb388
hariharan-km <hariharank6676@gmail.com>
2025-06-30 09:13:54 +0530

    Adding the files

--- backend/utils/GenerateToken.js
+++ backend/utils/GenerateToken.js
@@ -1,6 +1,16 @@
-require('dotenv').config()
-const jwt=require('jsonwebtoken')
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
-exports.generateToken=(payload,passwordReset=false)=>{
-    return jwt.sign(payload,process.env.SECRET_KEY,{expiresIn:passwordReset?process.env.PASSWORD_RESET_TOKEN_EXPIRATION:process.env.LOGIN_TOKEN_EXPIRATION})
-}
+exports.generateToken = (payload, passwordReset = false) => {
+  const secret = process.env.SECRET_KEY;
+
+  if (!secret) {
+    throw new Error("JWT secret key (SECRET_KEY) is not defined in .env");
+  }
+
+  const expiresIn = passwordReset
+    ? process.env.PASSWORD_RESET_TOKEN_EXPIRATION || '15m'
+    : process.env.LOGIN_TOKEN_EXPIRATION || '7d';
+
+  return jwt.sign(payload, secret, { expiresIn });
+};
